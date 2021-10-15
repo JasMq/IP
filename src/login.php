48bb4cbeb56c8879b36f04da012b3741f406d24a
JasMq <91511730+JasMq@users.noreply.github.com>
2021-10-15 19:21:22 +0800

    Add files via upload

--- src/login.php
+++ src/login.php
@@ -6,8 +6,8 @@
     <h1 class="login-heading">Log In</h1>
     <div class="login-form">
         <form action="/hidephp/login.hide.php" method="POST">
-            <input type="text" name="User Name" placeholder="Please enter the user name/email">
-            <input type="text" name="Password" placeholder="Please enter your password">
+            <input type="text" name="useraccount" placeholder="Please enter the user name/email">
+            <input type="text" name="password" placeholder="Please enter your password">
             <button type="submit" name="submit">Log In</button>
         </form>
 
@@ -15,5 +15,5 @@
 
     
 <?php
-    include_once 'botton.php';
+    include_once 'bottonbar.php';
 ?>
