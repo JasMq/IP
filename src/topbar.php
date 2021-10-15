909f99ca7a4b29daee66692754ba095a167f4fbf
JasMq <91511730+JasMq@users.noreply.github.com>
2021-10-15 13:58:22 +0800

    Create topbar.php

---
+++ src/topbar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <div class="container">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="login.php">Log in</a></li>
+                <li><a href="signup.php">Sign up</a></li>
+            </ul>
+        </div>
+    </nav>
